25e3fdb81148c195c0d4c9cc82a6b5857cb90396
John Pickard <jpickard@merkleinc.com>
2020-08-28 01:42:54 -0400

    missed refactoring

---
+++ modules/GT_StatesSetup.php
@@ -0,0 +1,14 @@
+<?php
+
+/* Collection of functions to handle states associated with setting up the game /round */
+
+require_once('GT_DBPlayer.php');
+
+class GT_StatesSetup extends APP_GameClass {
+    public function __construct() {
+    }
+
+
+}
+
+?>
